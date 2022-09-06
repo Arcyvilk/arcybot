a724b2b37a59702ac35de9bbd83e3e8f5eee36e1
Arcyvilk <blazemakerrr@gmail.com>
2022-09-06 21:06:08 +0200

    feat: 🎸 set up very base bot

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,14 @@
-export const main = () => {
-	return 'Hello world';
-};
+import { Client, GatewayIntentBits } from 'discord.js';
+import * as dotenv from 'dotenv';
+
+import { log } from 'utils';
+
+dotenv.config();
+
+const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+bot.once('ready', () => {
+	log.INFO('Ready!');
+});
+
+bot.login(process.env.DISCORD_TOKEN);
