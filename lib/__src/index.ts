5fa1d45aeb146af8d8d6620780f00815edf02c28
Arcyvilk <blazemakerrr@gmail.com>
2022-09-13 15:13:44 +0200

    feat: 🎸 create arcybot class

--- lib/__src/index.ts
+++ lib/__src/index.ts
@@ -1,32 +1,16 @@
-import { Client, GatewayIntentBits } from 'discord.js';
 import * as dotenv from 'dotenv';
+import Arcybot from 'Arcybot';
 
-import { CommandList } from 'CommandList';
-import { createCommandDictionary, log } from 'utils';
-
-import { mock as rawCommands } from '__src/commands/mock';
+import { mock as commandsObject } from '__src/commands/mock';
 import { help } from '__src/commands/commands';
 
 dotenv.config();
 
-const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
+const commandsFunctions = [help];
 
-const fnCommands = createCommandDictionary([help]);
-
-const Commands = new CommandList(rawCommands, fnCommands, {
+const bot = new Arcybot(commandsObject, commandsFunctions, {
 	discordToken: process.env.DISCORD_TOKEN,
 	botId: process.env.BOT_ID,
 });
 
-bot.once('ready', () => {
-	log.INFO('Ready!');
-	Commands.register();
-});
-
-bot.on('interactionCreate', async interaction => {
-	if (interaction.isChatInputCommand()) {
-		Commands.execute(interaction);
-	}
-});
-
-bot.login(process.env.DISCORD_TOKEN);
+bot.start();
