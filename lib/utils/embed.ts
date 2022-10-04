8157e5df18fc79f9c0cda7ca53184034edcec6a2
Arcyvilk <blazemakerrr@gmail.com>
2022-10-04 23:10:36 +0200

    fix: 🐛 add missing getXembed methods

--- lib/utils/embed.ts
+++ lib/utils/embed.ts
@@ -19,6 +19,48 @@ export const getSuccessEmbed = (
 	ephemeral: !!ephemeral,
 });
 
+export const getAwaitEmbed = (
+	title: string,
+	message: string,
+	ephemeral?: boolean,
+): CustomEmbed => ({
+	embeds: [
+		{
+			title: `⏳ ${title}`,
+			fields: [{ name: '---', value: message }],
+		},
+	],
+	ephemeral: !!ephemeral,
+});
+
+export const getInfoEmbed = (
+	title: string,
+	message: string,
+	ephemeral?: boolean,
+): CustomEmbed => ({
+	embeds: [
+		{
+			title: `ℹ️ ${title}`,
+			fields: [{ name: '---', value: message }],
+		},
+	],
+	ephemeral: !!ephemeral,
+});
+
+export const getWarningEmbed = (
+	title: string,
+	message: string,
+	ephemeral?: boolean,
+): CustomEmbed => ({
+	embeds: [
+		{
+			title: `⚠️ ${title}`,
+			fields: [{ name: '---', value: message }],
+		},
+	],
+	ephemeral: !!ephemeral,
+});
+
 export const getErrorEmbed = (
 	title: string,
 	message: string,
